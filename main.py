7c098d72b4a188819d9fb1e9b99468164b9e863b
Sderengo1219 <S.Derengowski@gmail.com>
2025-05-18 13:47:27 -0400

    added player object to the screen

--- main.py
+++ main.py
@@ -3,17 +3,21 @@
 #throughout this file
 import pygame
 from constants import *
-clock = pygame.time.Clock
+from circleshape import *
+from player import *
+clock = pygame.time.Clock()
 dt = 0
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while 1 != 2:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         dt = clock.tick(60) / 1000
         pygame.display.flip()
 
